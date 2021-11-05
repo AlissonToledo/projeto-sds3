aeb0270a1cb21546796e743bd7a86c0df6377799
Alisson Toledo <alisson.fptoledo.1990@gmail.com>
2021-11-04 22:24:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
@@ -7,7 +9,11 @@ function App() {
       <div className="container">
         <h1 className="text-primary">Olá mundo!</h1>
 
+        <DataTable />
+
       </div>
+
+      <Footer />
 
     </>
   );
